85571cd7cc7fc0bf30f2cedac41106fdcca5e92a
naufal <89512179+nomsf@users.noreply.github.com>
2024-12-11 22:58:06 +0700

    add singel item to cart test

--- src/test/java/org/example/PeriplusTest.java
+++ src/test/java/org/example/PeriplusTest.java
@@ -1,15 +1,19 @@
 package org.example;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Reporter;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.HashMap;
 
 public class PeriplusTest
 {
@@ -19,7 +23,7 @@ public class PeriplusTest
 
     private String EMAIL = "naufalindev@gmail.com";
     private String PASSWORD = "periplus@123";
-    private String productName;
+    private HashMap<String, Integer> cartItems = new HashMap<>();
 
     @BeforeTest
     public void Setup() {
@@ -37,7 +41,7 @@ public class PeriplusTest
     public void Login(){
         driver.findElement(By.xpath("/html/body/header/div[2]/div/div[1]/div[3]/div/div[3]")).click();
 
-//      Input Emaill
+//      Input Email
         driver.findElement(By.xpath("//*[@id=\"login\"]/div/table/tbody/tr[2]/td/input")).sendKeys(EMAIL);
 //      Input Password
         driver.findElement(By.xpath("//*[@id=\"ps\"]")).sendKeys(PASSWORD);
@@ -47,14 +51,40 @@ public class PeriplusTest
 
     @Test(dependsOnMethods = {"Login"})
     public void AddItemToCart(){
-        driver.findElement(By.xpath("/html/body/header/div[2]/div/div[1]/div[1]/div[1]/a")).click();
-        WebElement singleProduct = driver.findElement(By.xpath("//*[@id=\"content\"]/div[2]/div/div[2]/div/div[1]/div[1]/div/div[4]/div"));
 
-        productName = singleProduct.findElement(By.className("product-content product-contents")).findElement(By.xpath(".//h3/a")).getText();
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.className("preloader")));
+        WebElement homeButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/header/div[2]/div/div[1]/div[1]/div[1]")));
+        homeButton.click();
 
-        System.out.println(productName);
+        // FIND A SINGLE PRODUCT
+        //        driver.findElement(By.xpath("/html/body/header/div[2]/div/div[1]/div[1]/div[1]/a")).click();
+        WebElement singleProduct = driver.findElement(By.className("single-product"));
 
-        singleProduct.findElement(By.className("product-action")).findElement(By.xpath(".//a[1]")).click();
+        Reporter.log(singleProduct.getAttribute("outerHTML"));
+
+
+        // Get the href attribute value
+        WebElement linkElement = singleProduct.findElement(By.tagName("a"));
+        String hrefValue = linkElement.getAttribute("href");
+
+////*[@id="content"]/div[2]/div/div[2]/div/div[1]/div[1]/div/div[7]/div/div[1]/a
+
+        // EXTRACT ID FROM HREF VALUE
+        int startIndex = hrefValue.indexOf("/p/") + 3;
+        int endIndex = hrefValue.indexOf("/", startIndex);
+        String extractedValue = hrefValue.substring(startIndex, endIndex);
+
+        cartItems.put(extractedValue, 1);
+        Reporter.log("\n\nProduct Number ID: " + extractedValue);
+
+        // ADD ITEM TO CART
+        WebElement addToCartButton = singleProduct.findElement(By.className("addtocart"));
+
+        Reporter.log("\n\nAdd to Cart Button: " + addToCartButton.getAttribute("outerHTML"));
+
+        String jsFunction = addToCartButton.getAttribute("onclick");
+        JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+        jsExecutor.executeScript(jsFunction);
 
     }
 }
